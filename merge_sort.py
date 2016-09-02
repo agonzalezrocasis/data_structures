bd5bc10ff605864ef6f9afc81d633d860eb9289d
agonzalezrocasis <agonzalez@rocasis.mx>
2016-09-02 11:21:27 -0500

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
 	numbers = []
 
-	n = int(input('How many numbers would you like to sort?'))	
+	n = int(input('Numbers?'))	
 
 	for i in range(1, n+1, 1):
 		numbers.append(int(input('')))
